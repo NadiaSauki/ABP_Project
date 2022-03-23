a3052e158ca6fdcb9bdd78c05c811c692907d7b1
NadiaSauki <CB19045@student.ump.edu.my>
2022-03-23 10:27:58 +0700

    '_'

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css/style.css"> 
+
+    <title>Food Management System</title>
+    </head>
+
+    <body>
+        <ul>
+            <li><a href="#home">HOME</a></li>
+            <li><a href="#menu">MENU</a></li>
+            <li><a href="#order">ORDER</a></li>
+            <li style="float:right"><a href="#profile">PROFILE</a></li>
+            <li style="float:right"><a href="#cart">CART</a></li>
+        </ul>
+
+        <div class="slides">
+            <img src="" alt="meal" style="width:100%">
+        </div>
+    </body>
+</html>
