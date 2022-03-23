9c9fffc4e557f4f155bffe53fef04f63569ad94c
NadiaSauki <CB19045@student.ump.edu.my>
2022-03-23 10:44:01 +0700

    '_'

--- home.php
+++ home.php
@@ -9,14 +9,13 @@
     <body>
         <ul>
             <li><a href="#home">HOME</a></li>
-            <li><a href="#menu">MENU</a></li>
-            <li><a href="#order">ORDER</a></li>
+            <li><a href="#genre">GENRE</a></li>
             <li style="float:right"><a href="#profile">PROFILE</a></li>
             <li style="float:right"><a href="#cart">CART</a></li>
         </ul>
 
         <div class="slides">
-            <img src="" alt="meal" style="width:100%">
+            <img src="" alt="" style="width:100%">
         </div>
     </body>
 </html>
