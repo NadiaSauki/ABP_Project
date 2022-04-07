d4d4fb3728caeebf83bf7848cc17f056dcd8255e
NadiaSauki <CB19045@student.ump.edu.my>
2022-04-07 11:45:10 +0700

    changes

--- home.php
+++ home.php
@@ -12,11 +12,32 @@
     </head>
 
     <body>
-        <ul> <!--header-->
-            <li><a href="#home">Home</a></li>
-            <li><a href="#genre">Browse</a></li>
-            <li style="float:right"><a href="http://localhost/ABP_Project/login.php">Login</a></li>
-        </ul>
+        <header class="p-3 bg-dark text-white">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+                    </a>
+
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">About</a></li>
+                    </ul>
+
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                    </form>
+
+                    <div class="text-end">
+                    <a href="http://localhost/ABP_Project/login.php" class="btn btn-outline-light me-2" role="button">Login</a>
+                    <a href="http://localhost/ABP_Project/register.php" class="btn btn-warning" role="button">Register</a>
+                    </div>
+                </div>
+            </div>
+        </header>
 
         <!--slideshow-->
         <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
