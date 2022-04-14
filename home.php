471c0f7dc2d9007a2f1e42eed167c319e63fa199
NadiaSauki <CB19045@student.ump.edu.my>
2022-04-15 02:32:14 +0700

    change

--- home.php
+++ home.php
@@ -26,7 +26,7 @@
                     <li><a href="#">Genre</a></li>
                     <li><a href="#">Notification</a></li>
                     </ul>
-
+                    
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                     <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
                     </form>
