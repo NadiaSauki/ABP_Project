8793656b6da8a3417d3d0e940737553202578a5e
NadiaSauki <CB19045@student.ump.edu.my>
2022-04-17 14:01:56 +0700

    latest

--- login.php
+++ login.php
@@ -1,3 +1,44 @@
+<?php
+
+include 'config.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+
+      $row = mysqli_fetch_assoc($select_users);
+
+      if($row['user_type'] == 'admin'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         $_SESSION['admin_email'] = $row['email'];
+         $_SESSION['admin_id'] = $row['id'];
+         header('location:admin_page.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_email'] = $row['email'];
+         $_SESSION['user_id'] = $row['id'];
+         header('location:home.php');
+
+      }
+
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
+?>
+
+
 <!Doctype html>
 <html lang="en">
     <head>
@@ -9,6 +50,19 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/login.css">
     </head>
+
+    <?php
+    if(isset($message)){
+        foreach($message as $message){
+            echo '
+            <div class="message">
+                <span>'.$message.'</span>
+                <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+            </div>
+            ';
+        }
+    }
+    ?>
 
     <body>
         <section class="Form my-4 mx-5">
--- register.php
+++ register.php
@@ -1,3 +1,33 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $user_type = $_POST['user_type'];
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+      $message[] = 'user already exist!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$cpass', '$user_type')") or die('query failed');
+         $message[] = 'registered successfully!';
+         header('location:login.php');
+      }
+   }
+
+}
+
+?>
+
 <!Doctype html>
 <html lang="en">
     <head>
@@ -9,6 +39,19 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/register.css">
     </head>
+
+    <?php
+    if(isset($message)){
+        foreach($message as $message){
+            echo '
+            <div class="message">
+                <span>'.$message.'</span>
+                <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+            </div>
+            ';
+        }
+    }
+    ?>
 
     <body>
         <section class="Form my-4 mx-5">
@@ -25,12 +68,7 @@
                         <form>
                             <div class="form-row">
                                 <div class="col-lg-7">
-                                    <input type="firstname" placeholder="First Name" class="form-control my-3 p-4">
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col-lg-7">
-                                    <input type="lastname" placeholder="Last Name" class="form-control my-3 p-4">
+                                    <input type="name" placeholder="Your Name" class="form-control my-3 p-4">
                                 </div>
                             </div>
                             <div class="form-row">
@@ -43,10 +81,18 @@
                                     <input type="password" placeholder="Password" class="form-control my-3 p-4">
                                 </div>
                             </div>
+                            <div class="col-lg-7">
+                                <select name="user_type" class="form-control my-3 p-4">
+                                    <option selected>Select User Type</option>
+                                    <option value="user">User</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                            </div>
                             <div class="form-row">
                                 <div class="col-lg-7">
                                 <button type="button" class="btn2 mt-3 mb-5">Register</button><br>                                </div>
                             </div>
+                            <p>Already have an account? <a href="http://localhost/ABP_Project/login.php">Login here</a></p><br>
                         </form>
 
                     </div>
