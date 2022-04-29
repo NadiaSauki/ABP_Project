59d10ca740b8ad021942f22900a706147009215f
NadiaSauki <CB19045@student.ump.edu.my>
2022-04-29 17:04:14 +0700

    update ; |

--- manage_product/admin_product.php
+++ manage_product/admin_product.php
@@ -8,28 +8,27 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://kit.fontawesome.com/93a36b9820.js" crossorigin="anonymous"></script>
                 
-        <link rel="stylesheet" type="text/css" href="http://localhost/ABP_Project/css/home.css">
+        <link rel="stylesheet" type="text/css" href="http://localhost/ABP_Project/css/admin.css">
     </head>
 
     <body>
-        <header class="p-3 bg-dark text-white">
+        <header class="p-3 bg-light text-black">
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a href="http://localhost/ABP_Project/home.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"><h3>bookly.</h3>
+                    <a href="http://localhost/ABP_Project/manage_profile/admin_page.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"><h3 style="color: purple;">AdminPanel</h3>
                     <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">></svg>
                     </a>
 
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="http://localhost/ABP_Project/manage_shop/about.php">About</a></li>
-                        <li><a href="http://localhost/ABP_Project/manage_product/shop.php">Product</a></li>
-                        <li><a href="http://localhost/ABP_Project/manage_order/order.php">Orders</a></li>
+                        <li><a href="http://localhost/ABP_Project/manage_profile/admin_page.php">Home</a></li>
+                        <li><a href="http://localhost/ABP_Project/manage_product/admin_product.php">Products</a></li>
+                        <li><a href="http://localhost/ABP_Project/manage_order/admin_order.php">Orders</a></li>
+                        <li><a href="http://localhost/ABP_Project/manage_profile/admin_user.php">Users</a></li>
                     </ul>
 
                     <div class="text-end">
                         <ul>
-                            <li><a href="http://localhost/ABP_Project/manage_product/search_page.php"><i class="fas fa-search"></i> Search</a></li>
                             <li><a href="http://localhost/ABP_Project/manage_profile/user_profile.php"><i class="fa fa-user"></i> Profile</a></li>
-                            <li><a href="http://localhost/ABP_Project/manage_cart/add_to_cart.php"><i class="fas fa-shopping-cart"></i> Cart</a></li>
                             <li><a href="http://localhost/ABP_Project/logout.php">Sign out</a></li>
                         </ul>    
                     </div>
@@ -37,7 +36,7 @@
             </div>
         </header>
 
-        <div class="p-3 bg-dark text-white">
+        <div class="p-3 bg-light text-black">
             <div class="container">
                 <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 my-3 border-top">
                     <p class="col-md-4 mb-0 text-muted">&copy; BOOKSHOP 2022 | All Rights Reserved </p>
