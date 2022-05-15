5f46f17a486613a0f74cb9dea33dab6903ba077a
Fahmi <zlfhmi14@gmail.com>
2022-05-15 23:04:36 +0700

    qwertttu

--- manage_order/admin_order.php
+++ manage_order/admin_order.php
@@ -30,6 +30,7 @@ if(isset($_GET['delete'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Pejuuu</title>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
