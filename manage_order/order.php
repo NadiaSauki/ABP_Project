2abc5a7ca18f4658da4e37a39ab27249ee281c8d
fahmi <zlfhmi14@gmail.com>
2022-05-16 00:00:25 +0700

    abu

--- manage_order/order.php
+++ manage_order/order.php
@@ -26,6 +26,44 @@ session_start();
 
 </head>
 <body>
+   <!-- HEADER -->
+   <header class="p-3 bg-dark text-white">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="http://localhost/ABP_Project/home.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"><h3 style="color: purple;">bookly.</h3>
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">></svg>
+                    </a>
+
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="http://localhost/ABP_Project/manage_shop/about.php">About</a></li>
+                        <li><a href="http://localhost/ABP_Project/manage_product/shop.php">Product</a></li>
+                        <li><a href="http://localhost/ABP_Project/manage_order/order.php">Orders</a></li>
+                    </ul>
+
+                    <div class="text-end">
+                        <ul>
+                            <li><a href="http://localhost/ABP_Project/manage_product/search_page.php"><i class="fas fa-search"></i> Search</a></li>
+                            <li><a href="http://localhost/ABP_Project/manage_profile/user_profile.php"><i class="fa fa-user"></i> Profile</a></li>
+                            <li><a href="http://localhost/ABP_Project/manage_cart/add_to_cart.php"><i class="fas fa-shopping-cart"></i> Cart</a></li>
+                            <li><a href="http://localhost/ABP_Project/logout.php">Sign out</a></li>
+                        </ul>    
+                    </div>
+                </div>
+            </div>
+        </header>
+
+    <!-- FOOTER -->
+    <div class="p-3 bg-dark text-white">
+            <div class="container">
+                <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 my-3 border-top">
+                    <p class="col-md-4 mb-0 text-muted">&copy; BOOKSHOP 2022 | All Rights Reserved </p>
+
+                    <ul class="nav col-md-4 justify-content-end">
+                    <li class="nav-item"><a href="http://localhost/ABP_Project/home.php" class="nav-link px-2 text-muted">Home</a></li>
+                    <li class="nav-item"><a href="http://localhost/ABP_Project/manage_shop/contact.php" class="nav-link px-2 text-muted">Contact</a></li>
+                    <li class="nav-item"><a href="http://localhost/ABP_Project/manage_shop/about.php" class="nav-link px-2 text-muted">About</a></li>
+                    </ul>
+                </footer>    
    
 <?php include 'header.php'; ?>
 
