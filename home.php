5ea68eaaf844f64954cbed9ded2ea40be1630e72
Iman <imanurul20@gmail.com>
2022-05-17 15:05:05 +0700

    lolo

--- home.php
+++ home.php
@@ -53,7 +53,7 @@ if(isset($_POST['add_to_cart'])){
 
    <div class="content">
       <h3>Hand Picked Book to your door.</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quod? Reiciendis ut porro iste totam.</p>
+      <p>Chiciko.</p>
       <a href="about.php" class="white-btn">discover more</a>
    </div>
 
