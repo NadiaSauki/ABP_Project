f34bf27335b6618d14e5080d9d47f32b805dc687
fasihah <fasihahayuni01@gmail.com>
2022-05-17 16:09:53 +0800

    update

--- footer.php
+++ footer.php
@@ -3,39 +3,38 @@
    <div class="box-container">
 
       <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php">home</a>
-         <a href="about.php">about</a>
-         <a href="shop.php">shop</a>
-         <a href="contact.php">contact</a>
+         <h3>Shortcut</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About Us</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
       </div>
 
       <div class="box">
-         <h3>extra links</h3>
-         <a href="login.php">login</a>
-         <a href="register.php">register</a>
-         <a href="cart.php">cart</a>
-         <a href="orders.php">orders</a>
+         <h3>Shortcut</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
       </div>
 
       <div class="box">
          <h3>contact info</h3>
-         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
-         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
-         <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
-         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
+         <p> Customer Care : </p>
+         <a href= "tel:+60134567890"><i class="fas fa-phone"></i> +6013-456-7890 </a>
+         <p> Product Related : </p>
+         <a href= "tel:+60182223333"><i class="fas fa-phone"></i> +6018-222-3333 </a>
+         <a href= "https://accounts.google.com/b/1/AddMailService"><i class="fas fa-envelope"></i> juBook@gmail.com </a>
+         <a href= "https://www.google.com/maps/place/Selangor,+Malaysia/@3.2322231,101.3809674,9z/data=!3m1!4b1!4m5!3m4!1s0x31cc4252cdeb045f:0x26e1bee1215dfbb9!8m2!3d3.0738379!4d101.5183469"> <i class="fas fa-map-marker-alt"></i> Selangor, Malaysia</a>
       </div>
 
       <div class="box">
          <h3>follow us</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i> @ju's_Book </a>
+         <a href="https://twitter.com/i/flow/login"> <i class="fab fa-twitter"></i> @juBook_Store </a>
+         <a href="https://www.instagram.com/?hl=en"> <i class="fab fa-instagram"></i> @juBookMalaysia</a>
+         <a href="https://www.linkedin.com/"> <i class="fab fa-linkedin"></i> @juBook </a>
       </div>
 
    </div>
-
-   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
-
 </section>
