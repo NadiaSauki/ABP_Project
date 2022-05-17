1e71a4a87395c4cc46830766ea1d487c4caaf95d
Iman <imanurul20@gmail.com>
2022-05-17 15:14:47 +0700

    mumu

--- login.php
+++ login.php
@@ -72,8 +72,8 @@ if(isset($message)){
 
    <form action="" method="post">
       <h3>login now</h3>
-      <input type="email" name="email" placeholder="enter your email" required class="box">
-      <input type="password" name="password" placeholder="enter your password" required class="box">
+      <input type="email" name="email" placeholder="Enter Your Email" required class="box">
+      <input type="password" name="password" placeholder="Enter Your Password" required class="box">
       <input type="submit" name="submit" value="login now" class="btn">
       <p>don't have an account? <a href="register.php">register now</a></p>
    </form>
