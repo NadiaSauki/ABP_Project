1a3671d20d4f672d6c1a09fe363f8f17a5f3560f
Iman <imanurul20@gmail.com>
2022-05-17 15:41:22 +0700

    yuyu

--- home.php
+++ home.php
@@ -52,8 +52,8 @@ if(isset($_POST['add_to_cart'])){
 <section class="home">
 
    <div class="content">
-      <h3>Choose your favourite books here.</h3>
-      <p>Chiciko.</p>
+      <h3>WELCOME TO juBook.</h3>
+      <p>Find your favourite books here</p>
       <a href="about.php" class="white-btn">discover more</a>
    </div>
 
